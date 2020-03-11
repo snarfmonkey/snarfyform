35009fe7dfcf7154928f9ad987a1e69777d4db60
Aaron Sharpe <Aaron.sharpe@cirium.com>
2020-03-11 15:57:23 -0700

    ADD ASSUMEROLE POLICY

--- main.tf
+++ main.tf
@@ -5,4 +5,20 @@ resource "aws_iam_role" "snarfytest" {
     Test        = "true"
     Deleteme    = "please"
   }
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }

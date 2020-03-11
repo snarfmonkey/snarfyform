bc77359bb5319410715f8935717b397733b0f1a6
Aaron Sharpe <Aaron.sharpe@cirium.com>
2020-03-11 16:12:04 -0700

    just kidding.

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-resource "aws_iam_role" "snarfytest" {
-  name          = "snarfytest"
-  description   = "snarfytest IAM role that does nothing."
-  tags          = {
-    Test        = "true"
-    Deleteme    = "please"
-  }
+# resource "aws_iam_role" "snarfytest" {
+#   name          = "snarfytest"
+#   description   = "snarfytest IAM role that does nothing."
+#   tags          = {
+#     Test        = "true"
+#     Deleteme    = "please"
+#   }
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": "ec2.amazonaws.com"
+#       },
+#       "Effect": "Allow",
+#       "Sid": ""
+#     }
+#   ]
+# }
+# EOF
+# }
